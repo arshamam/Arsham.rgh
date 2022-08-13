4a794bf5e1b2200de1e44a8f0d42ab02920b4821
Arsham.rgh <108665376+arshamam@users.noreply.github.com>
2022-08-13 12:41:11 +0430

    Add files via upload

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const bodyparser = require("body-parser");
+const ejs = require("ejs");
+
+const app = express()
+
+app.set('view engine', 'ejs');
+app.use(bodyparser.urlencoded({
+  extended: true
+}));
+app.use(express.static("public"));
+
+app.get("/", function(req, res) {
+    res.render("about-me.ejs");
+});
+
+app.get("/supermarket", function(req, res) {
+  res.render("super.ejs");
+});
+
+app.get("/contact-me", function(req, res) {
+  res.render("contact-me.ejs");
+});
+app.get("/about-site", function(req, res) {
+  res.render("about-site.ejs");
+});
+
+
+
+
+
+app.listen(3000, function() {
+    console.log("server started in port 3000")
+});
